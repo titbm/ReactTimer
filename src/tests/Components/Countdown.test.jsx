1876f6ce08b34cf1a98043ab7f0c7e09a90be62e
Igor Popikov <titbm@yandex.ru>
2016-11-24 02:35:04 +0300

    Added tests for Countdown components

--- src/tests/Components/Countdown.test.jsx
+++ src/tests/Components/Countdown.test.jsx
@@ -26,6 +26,7 @@ describe('Countdown', () => {
         1001
       );
     });
+
     it('should never set count less than zero', (done) => {
       var countdown = TestUtils.renderIntoDocument(<Countdown />);
       countdown.handleSetCountdown(1);
@@ -35,6 +36,34 @@ describe('Countdown', () => {
           done();
         },
         2001
+      );
+    });
+
+    it('should paused countdown on paused status', (done) => {
+      var countdown = TestUtils.renderIntoDocument(<Countdown />);
+      countdown.handleSetCountdown(3);
+      countdown.handleStatusChange('paused');
+      setTimeout(
+        () => {
+          expect(countdown.state.count).toBe(3);
+          expect(countdown.state.countdownStatus).toBe('paused');
+          done();
+        },
+        1001
+      );
+    });
+
+    it('should reset count on stopped', (done) => {
+      var countdown = TestUtils.renderIntoDocument(<Countdown />);
+      countdown.handleSetCountdown(3);
+      countdown.handleStatusChange('stopped');
+      setTimeout(
+        () => {
+          expect(countdown.state.count).toBe(0);
+          expect(countdown.state.countdownStatus).toBe('stopped');
+          done();
+        },
+        1001
       );
     });
   });
